094ea84327a6fda810bdb220a780823a2b3f9061
V <ramyavenkatesh182@gmail.com>
2020-01-13 16:22:08 +0530

    Adding the code of author quiz of UI and events

--- src/index.js
+++ src/index.js
@@ -1,12 +1,84 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import AuthorQuiz from './AuthorQuiz';
 import * as serviceWorker from './serviceWorker';
+import {shuffle, sample} from 'underscore';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const authors =[
+    {
+        name: 'Mark Twain',
+        imageUrl: 'images/authors/marktwain.jpg',
+        imageSource: 'Wikimedia commons',
+        books: ['The Adventures of Huckleberry Fin',
+                'Life on the Mississippi',
+            'Roughing It']
+    },
+    {
+        name: 'Charles Dickens',
+        imageUrl: 'images/authors/charlesdickens.jpg',
+        imageSource: 'Wikimedia commons',
+        books: ['A Christmas Carol',
+            'Oliver Twist']
+    },
+    {
+        name: 'J K Rowling',
+        imageUrl: 'images/authors/jkrowling.jpg',
+        imageSource: 'Wikimedia commons',
+        books: ['Harry Potter and the Philosophers Stone']
+    },
+    {
+        name: 'Joseph Conrad',
+        imageUrl: 'images/authors/JosephConrad.png',
+        imageSource: 'Wikimedia commons',
+        books: ['Heart of Darkness',
+                'Lord Jim']
+    },
+    {
+        name: 'Shakespeare',
+        imageUrl: 'images/authors/shakespeare.jpg',
+        imageSource: 'Wikimedia commons',
+        books: ['The Phoenix and the turtle']
+    },
+    {
+        name: 'Strephen King',
+        imageUrl: 'images/authors/stephenKing.jpg',
+        imageSource: 'Wikimedia commons',
+        books: ['The Shining',
+                'Doctor Sleep']
+    }
+];
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+function getTurnData(authors){
+    const allBooks = authors.reduce(function(p,c,i){
+        return p.concat(c.books);
+    }, []);
+    const fourRandomBooks = shuffle(allBooks).slice(0,4);
+    const answer = sample(fourRandomBooks)
+
+    return {
+        books: fourRandomBooks,
+        author: authors.find((author)=>
+        author.books.some((title)=>
+        title===answer))
+    }
+}
+
+const state = {
+    turnData: getTurnData(authors),
+    highlight: 'correct'
+    
+};
+
+function onAnswerSelected(answer){
+    const isCorrect = state.turnData.author.books.some((book)=>book === answer); 
+    state.highlight= isCorrect? 'correct': 'wrong';
+    render();
+}
+
+function render(){
+    ReactDOM.render(<AuthorQuiz {...state} onAnswerSelected={onAnswerSelected}/>, document.getElementById('root')); 
+}
+
+render();
 serviceWorker.unregister();
